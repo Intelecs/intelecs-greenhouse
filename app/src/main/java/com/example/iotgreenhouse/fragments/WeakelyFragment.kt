6d193f7e68b7cdf8cf4ba79f3182a7b8e1869834
stormryder <january_thomas@hotmail.com>
2020-01-15 16:43:45 +0300

    IoT Green House - Material Bottom Sheets

--- app/src/main/java/com/example/iotgreenhouse/fragments/WeakelyFragment.kt
+++ app/src/main/java/com/example/iotgreenhouse/fragments/WeakelyFragment.kt
@@ -5,15 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.anychart.AnyChart
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
-import com.anychart.enums.Anchor
-import com.anychart.enums.HoverMode
-import com.anychart.enums.Position
-import com.anychart.enums.TooltipPositionMode
 import com.example.iotgreenhouse.R
-import kotlinx.android.synthetic.main.weakely_fragment.view.*
+import kotlinx.android.synthetic.main.weakely_fragment.*
 
 
 class WeakelyFragment: Fragment() {
@@ -22,44 +15,18 @@ class WeakelyFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View =  inflater.inflate(R.layout.weakely_fragment, container, false)
+        val view: View = inflater.inflate(R.layout.weakely_fragment, container, false)
 
-        val cartesian = AnyChart.column()
-        val data: MutableList<DataEntry> = ArrayList()
-        data.add(ValueDataEntry("Monday", 24))
-        data.add(ValueDataEntry("Tuesday", 20))
-        data.add(ValueDataEntry("Wednesday", 24))
-        data.add(ValueDataEntry("Thursday", 24))
-        data.add(ValueDataEntry("Friday", 25))
-        data.add(ValueDataEntry("Saturday", 19))
-        data.add(ValueDataEntry("Sunday", 15))
+        val weeklyTempratureFagement = WeeklyTempratureFagement()
+        val tempratureFagement = fragmentManager!!.beginTransaction()
+        tempratureFagement.add(R.id.temp_frame, weeklyTempratureFagement)
+        tempratureFagement.commit()
 
-        val column = cartesian.column(data)
-        column.tooltip()
-            .titleFormat("{%X}")
-            .position(Position.CENTER_BOTTOM)
-            .anchor(Anchor.CENTER_BOTTOM)
-            .offsetX(0.0)
-            .offsetY(5.0)
-            .format("{%Value}{groupsSeparator: } ℃")
+        val weeklyMoistureFragment = WeeklyMoistureFragment()
+        val moistureFragment = fragmentManager!!.beginTransaction()
+        moistureFragment.add(R.id.moist_frame, weeklyMoistureFragment)
+        moistureFragment.commit()
 
-        cartesian.animation(true)
-        cartesian.title("Weakly Readings of Temperature")
-
-        cartesian.yScale().minimum(0.0)
-
-        cartesian.yAxis(0).labels().format("℃ {%Value}{groupsSeparator: }")
-
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT)
-        cartesian.interactivity().hoverMode(HoverMode.BY_X)
-
-        cartesian.xAxis(0).title("Days")
-        cartesian.yAxis(0).title("Temperature (in Celsius)")
-        view.weekly_chart.setBackgroundColor("#333131")
-        view.weekly_chart.setProgressBar(view.progress_view)
-        view.weekly_chart.setBackgroundColor("#333131")
-        view.weekly_chart.setZoomEnabled(true)
-        view.weekly_chart.setChart(cartesian)
         return view
 
     }
