bac1a6b16de88e61c3ff8e620ade83edc0083e7e
stormryder <january_thomas@hotmail.com>
2020-01-15 13:14:29 +0300

    IoT Green House - Material Bottom Sheets

--- app/src/main/java/com/example/iotgreenhouse/activities/MainActivity.kt
+++ app/src/main/java/com/example/iotgreenhouse/activities/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.example.iotgreenhouse.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.iotgreenhouse.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        edit_threshold.setOnClickListener {
+            val viewInflated:View = LayoutInflater.from(this).inflate(R.layout.threshold_layout, null)
+            val bottomSheetDialog = BottomSheetDialog(this)
+            bottomSheetDialog.setOnCancelListener {
+
+            }
+            bottomSheetDialog.setTitle(getString(R.string.threshold_dialog_title))
+            bottomSheetDialog.setContentView(viewInflated)
+            bottomSheetDialog.show()
+        }
 
         btn_navigation.setOnItemSelectedListener {
             when (it) {
