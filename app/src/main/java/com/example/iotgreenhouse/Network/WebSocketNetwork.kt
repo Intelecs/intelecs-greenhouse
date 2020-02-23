07bf8ea1f929983e42f75bc7debea6b6c75a4836
stormryder <january_thomas@hotmail.com>
2020-02-23 16:47:02 +0300

    IoT Green House - Web Socket Controls

--- app/src/main/java/com/example/iotgreenhouse/Network/WebSocketNetwork.kt
+++ app/src/main/java/com/example/iotgreenhouse/Network/WebSocketNetwork.kt
@@ -1,38 +1,70 @@
 package com.example.iotgreenhouse.Network
 
 import android.util.Log
+import com.tinder.scarlet.Message
+import com.tinder.scarlet.websocket.ShutdownReason
+import com.tinder.scarlet.websocket.WebSocketEvent
+import io.reactivex.Flowable
+import io.reactivex.processors.PublishProcessor
 import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString
 
 class WebSocketNetwork : WebSocketListener() {
+    var isConnected = false
+    private val processor = PublishProcessor.create<WebSocketEvent>().toSerialized()
+    fun observe(): Flowable<WebSocketEvent> = processor
+    fun terminate() = processor.onComplete()
     override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
         super.onClosed(webSocket, code, reason)
-        Log.d("WebSocket", "Error code $code, $reason")
+//        Log.d("WebSocket", "Error code $code, $reason")
+        isConnected = false
+        processor.onNext(WebSocketEvent.OnConnectionClosed(ShutdownReason(code, reason)))
 
     }
 
     override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
         webSocket.close(1000, null)
         webSocket.cancel()
-        Log.d("WebSocket", "Closing $code, $reason")
+        isConnected = false
+//        Log.d("WebSocket", "Closing $code, $reason")
+        processor.onNext(WebSocketEvent.OnConnectionClosing(
+            ShutdownReason(code, reason)
+        ))
     }
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
         super.onOpen(webSocket, response)
-        Log.d("WebSocket", "Network Opened $response")
+//        Log.d("WebSocket", "Network Opened $response")
+        isConnected = true
+        processor.onNext(WebSocketEvent.OnConnectionOpened(
+            webSocket, response
+        ))
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
         super.onMessage(webSocket, text)
-        Log.d("WebSocket", "Message received $text")
+        isConnected = false
+//        Log.d("WebSocket", "Message received $text")
+        processor.onNext(WebSocketEvent.OnMessageReceived(
+            Message.Text(text)
+        ))
     }
 
     override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
         super.onMessage(webSocket, bytes)
-        Log.d("WebSocket", "Message received $bytes")
+//        Log.d("WebSocket", "Message received $bytes")
+        processor.onNext(WebSocketEvent.OnMessageReceived(
+            Message.Bytes(bytes.toByteArray())
+        ))
     }
+    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        processor.onNext(WebSocketEvent.OnConnectionFailed(t))
+        isConnected = false
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/iotgreenhouse/activities/MainActivity.kt
+++ app/src/main/java/com/example/iotgreenhouse/activities/MainActivity.kt
@@ -2,52 +2,99 @@ package com.example.iotgreenhouse.activities
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
+import android.view.animation.AnimationUtils
 import android.view.animation.RotateAnimation
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.iotgreenhouse.Network.WebSocketClient
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.example.iotgreenhouse.Network.WebSocketNetwork
 import com.example.iotgreenhouse.R
-import com.example.iotgreenhouse.model.SendControls
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.tinder.scarlet.websocket.WebSocketEvent
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_toolbar.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
 
 
 class MainActivity : AppCompatActivity() {
 
     var client: OkHttpClient = OkHttpClient.Builder().build()
+    var webSocket:WebSocket? = null
+    var sensorClient:WebSocket? = null
+
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val request = Request.Builder().url("ws://192.168.43.66:8001/control-devices").build()
-        val sensorListner = Request.Builder().url("ws://192.168.43.66:8001/sensors").build()
+        val sensorRequest = Request.Builder().url("ws://192.168.43.66:8001/sensors").build()
 
         val webSocketListener = WebSocketNetwork()
-        val webSocket = client.newWebSocket(request, webSocketListener)
-        val sensorClient = client.newWebSocket(sensorListner, webSocketListener)
-        client.dispatcher.executorService.execute {
+        webSocket = client.newWebSocket(request, webSocketListener)
+        sensorClient = client.newWebSocket(sensorRequest, webSocketListener)
 
+
+        webSocketListener.observe()
+            .subscribe {
+                if (it is WebSocketEvent.OnConnectionOpened) {
+                    Log.d("WebSocket Open:", "$it")
+                    runOnUiThread {
+                        if (connection_refresh.isVisible) {
+                            connection_refresh.visibility = View.GONE
+                        }
+                        connection_image.setColorFilter(applicationContext.resources.getColor(R.color.colorPrimary))
+                        connection_hint.text = getText(R.string.connected)
+                    }
+                } else if (it is WebSocketEvent.OnMessageReceived) {
+                    Log.d("WebSocket Message:", "${it.message}")
+                } else if (it is WebSocketEvent.OnConnectionClosing) {
+                    Log.d("WebSocket Closing:", "$it")
+                    runOnUiThread {
+                        if (!connection_refresh.isVisible) {
+                            connection_refresh.visibility = View.VISIBLE
+                        }
+                        connection_image.setColorFilter(applicationContext.resources.getColor(R.color.ligh_gray))
+                        connection_hint.text = getText(R.string.not_connected)
+                    }
+                } else if (it is WebSocketEvent.OnConnectionClosed) {
+                    Log.d("WebSocket Closed:", "$it")
+                    runOnUiThread {
+                        if (!connection_refresh.isVisible) {
+                            connection_refresh.visibility = View.VISIBLE
+                        }
+
+                        connection_image.setColorFilter(applicationContext.resources.getColor(R.color.ligh_gray))
+                        connection_hint.text = getText(R.string.not_connected)
+                    }
+                }
+            }
+
+        connection_refresh.setOnClickListener {
+            val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.refresh_rotate)
+            refresh.startAnimation(rotateAnimation)
+            webSocket = client.newWebSocket(request, webSocketListener)
+            sensorClient = client.newWebSocket(sensorRequest, webSocketListener)
         }
-        Log.d("Websocke", "Request ${webSocket.request().body}")
+
 
 //        val sendControls = SendControls("FAN", "ON")
 //
 //        WebSocketClient.iotService.observeNetwork().filter {
 //            it is WebSocketEvent.OnConnectionOpened
 //        }.subscribe{
-//            Log.d("Message", "Connected $it")
+//
 //        }
 //
 //
@@ -56,10 +103,9 @@ class MainActivity : AppCompatActivity() {
 //        }
 
         fan_switch.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(isChecked) {
+            if (isChecked) {
                 Log.d("Message", "${isChecked}")
-                webSocket.send("hello")
-
+                webSocket!!.send("hello")
 //                WebSocketClient.iotService.observeNetwork().filter {
 //                    it is WebSocketEvent.OnConnectionOpened
 //                }.subscribe{ WebSocketClient.iotService.sendControl(sendControls)}
@@ -67,11 +113,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         connection_holder.setOnClickListener {
-            val rotateAnimation = RotateAnimation(
-                0f, 180f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
-            )
-            rotateAnimation.setInterpolator(LinearInterpolator())
-            refresh.startAnimation(rotateAnimation)
+            val animation = AnimationUtils.loadAnimation(this, R.anim.refresh_rotate)
+            refresh.startAnimation(animation)
         }
 
 
